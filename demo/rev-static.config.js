b0a53a7090def7a4e8ba2445c864a39c4ee44bc7
york yao <yaoao12306@outlook.com>
2017-07-09 11:24:43 +0800

    remove unused rev-static config

--- demo/rev-static.config.js
+++ demo/rev-static.config.js
@@ -5,16 +5,11 @@ module.exports = {
     'demo/*.bundle.css'
   ],
   outputFiles: file => file.replace('.ejs', ''),
-  json: false,
   ejsOptions: {
     rmWhitespace: true
   },
   sha: 256,
   customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + '-' + md5String + extensionName,
-  noOutputFiles: [],
-  es6: false,
-  less: false,
-  scss: false,
   base: 'demo',
   fileSize: 'demo/file-size.json'
 }
