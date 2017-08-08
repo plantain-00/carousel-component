e185c3f550ae22dc4058c1f73d64e5c3c32a4995
york yao <yaoao12306@outlook.com>
2017-08-08 20:09:11 +0800

    improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,28 +1,36 @@
 module.exports = {
   build: [
-    `rimraf dist && rimraf demo/**/index.bundle-*.js`,
-    `file2variable-cli src/vue.template.html -o src/variables.ts --html-minify --base src`,
-    `tsc -p src`,
-    `tsc -p demo`,
-    `lessc src/carousel.less > dist/carousel.css`,
-    `cleancss -o dist/carousel.min.css dist/carousel.css`,
-    `cleancss -o demo/index.bundle.css dist/carousel.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`,
-    `webpack --display-modules --config demo/webpack.config.js`,
+    `rimraf dist`,
+    `mkdirp dist`,
+    {
+      js: [
+        `file2variable-cli src/vue.template.html -o src/variables.ts --html-minify --base src`,
+        `tsc -p src`,
+        `tsc -p demo`,
+        `webpack --display-modules --config demo/webpack.config.js`
+      ],
+      css: [
+        `lessc src/carousel.less > dist/carousel.css`,
+        `cleancss -o dist/carousel.min.css dist/carousel.css`,
+        `cleancss -o demo/index.bundle.css dist/carousel.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
+      ],
+      clean: `rimraf demo/**/index.bundle-*.js`
+    },
     `rev-static --config demo/rev-static.config.js`
   ],
-  lint: [
-    `tslint "src/**/*.ts" "src/**/*.tsx" "demo/**/*.ts" "demo/**/*.tsx"`,
-    `standard "**/*.config.js"`,
-    `stylelint "src/**/*.less"`
-  ],
+  lint: {
+    ts: `tslint "src/**/*.ts" "src/**/*.tsx" "demo/**/*.ts" "demo/**/*.tsx"`,
+    js: `standard "**/*.config.js"`,
+    less: `stylelint "src/**/*.less"`
+  },
   test: [
     'tsc -p spec',
     'karma start spec/karma.config.js'
   ],
-  fix: [
-    `standard --fix "**/*.config.js"`
-  ],
-  release: [
-    `clean-release`
-  ]
+  fix: {
+    ts: `tslint --fix "src/**/*.ts" "src/**/*.tsx" "demo/**/*.ts" "demo/**/*.tsx"`,
+    js: `standard --fix "**/*.config.js"`,
+    less: `stylelint --fix "src/**/*.less"`
+  },
+  release: `clean-release`
 }
