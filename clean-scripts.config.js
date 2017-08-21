3d1ec5058e458d6670cb56f8d35768011e060e2c
york yao <yaoao12306@outlook.com>
2017-08-21 09:49:08 +0800

    improve watch script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -49,5 +49,12 @@ module.exports = {
     less: `stylelint --fix "src/**/*.less"`
   },
   release: `clean-release`,
-  watch: `watch-then-execute "src/**/*.ts" "src/**/*.tsx" "demo/**/*.ts" "demo/**/*.tsx" "src/**/*.template.html" --exclude "src/*-variables.ts" --script "npm run build"`
+  watch: {
+    vue: `file2variable-cli src/vue.template.html -o src/variables.ts --html-minify --base src --watch`,
+    src: `tsc -p src --watch`,
+    demo: `tsc -p demo --watch`,
+    webpack: `webpack --config demo/webpack.config.js --watch`,
+    less: `watch-then-execute "src/*.less" --script "clean-scripts build[2].css"`,
+    rev: `rev-static --config demo/rev-static.config.js --watch`
+  }
 }
