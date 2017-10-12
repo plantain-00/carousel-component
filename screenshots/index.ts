0d70a62930f1e825189f73a3e4a8893a0119c561
york yao <yaoao12306@outlook.com>
2017-10-12 16:58:31 +0800

    improve test

--- screenshots/index.ts
+++ screenshots/index.ts
@@ -7,7 +7,32 @@ import * as puppeteer from "puppeteer";
 
     for (const type of ["vue", "react"]) {
         await page.goto(`http://localhost:8000/demo/${type}`);
+        await page.waitFor(500);
         await page.screenshot({ path: `screenshots/${type}-initial.png`, fullPage: true });
+
+        await page.waitFor(3000);
+        await page.screenshot({ path: `screenshots/${type}-3s.png`, fullPage: true });
+
+        await page.waitFor(3000);
+        await page.screenshot({ path: `screenshots/${type}-6s.png`, fullPage: true });
+
+        const right = await page.$(".right");
+        await right.hover();
+        await page.waitFor(100);
+        await page.screenshot({ path: `screenshots/${type}-right-hover.png`, fullPage: true });
+
+        await right.click();
+        await page.waitFor(500);
+        await page.screenshot({ path: `screenshots/${type}-right-click.png`, fullPage: true });
+
+        const left = await page.$(".left");
+        await left.hover();
+        await page.waitFor(100);
+        await page.screenshot({ path: `screenshots/${type}-left-hover.png`, fullPage: true });
+
+        await left.click();
+        await page.waitFor(500);
+        await page.screenshot({ path: `screenshots/${type}-left-click.png`, fullPage: true });
     }
 
     browser.close();
